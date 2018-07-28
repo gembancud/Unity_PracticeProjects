bbd553d5e87a1b3cca821e65e200899fc40fe461
gembancud <gembancud@gmail.com>
2018-07-29 03:30:32 +0800

    RollABall Project Added

---
+++ RollABallTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody Player;
+
+    public Text countText;
+    public Text winText;
+
+    public float speed= 50f;
+    private int count;
+
+    void Start()
+    {
+        Player = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        Player.AddForce(movement* speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 9) winText.text = "You Win!";
+
+    }
+
+}
---
+++ RollABallTutorial/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float TurnSpeed;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 45, 0)*TurnSpeed * Time.deltaTime);
+	}
+}
